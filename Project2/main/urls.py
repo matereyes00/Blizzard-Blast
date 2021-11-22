d11153a936d2eebb5285f74b4f77c06dc535dd9f
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-11-22 12:25:25 +0800

    Initial Commit on MateBranch

---
+++ Project2/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('order', order, name='order'),
+    path('inventory', inventory, name='inventory'),
+    path('schedule', schedule, name='schedule'),
+    path('report', report, name='report'),
+]
