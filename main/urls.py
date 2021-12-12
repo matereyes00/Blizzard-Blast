cfbc42696736515a38b7ea3c5cc68912a9f0c0d8
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-12-13 02:05:39 +0800

    Created new views and urls.

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('schedule', schedule, name='schedule'),
     path('report', report, name='report'),
     path('receipt', receipt, name='receipt'),
-    path('addorder', addorder, name='addorder' ),
+    path('addorder', addorder, name='addorder'),
+    path('manager_query', manager_query, name='manager_query'),
+    path('week_query', week_query, name = 'week_query'),
+    path('role_query', role_query, name = 'role_query')
     
 ]
