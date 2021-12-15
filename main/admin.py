291eb2fa3c42899f47506930e5e13d9e14cdf767
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-12-15 16:51:53 +0800

    Added admin views

--- main/admin.py
+++ main/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+
+admin.site.register(Orders)
 admin.site.register(Employee)
 admin.site.register(EmployeeRole)
+admin.site.register(Customer)
+admin.site.register(BaseFlavor)
+admin.site.register(Size)
+
+admin.site.register(Item)
+admin.site.register(Ingredient)
