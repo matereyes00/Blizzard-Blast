0e523463973587258e95693ffa3d8aceebc46ded
Matê Reyes <martina.therese.reyes@student.ateneo.edu>
2025-01-28 17:52:46 +0800

    modified files to make it work on local development

--- blizzardblast/settings.py
+++ blizzardblast/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'blizzardblast.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
